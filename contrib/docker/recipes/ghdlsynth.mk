9ba7657be8220d674e61ece03fc7a4cc7a13328a
Martin <hackfin@section5.ch>
2020-04-01 21:11:59 +0200

    URL change of ghdl yosys plugin

--- contrib/docker/recipes/ghdlsynth.mk
+++ contrib/docker/recipes/ghdlsynth.mk
@@ -1,20 +1,23 @@
 include config.mk
 
-GHDLSYNTH_URL = https://github.com/tgingold/ghdlsynth-beta.git
+COMMIT = e3cbdbca7d4fb3464e9456d627a5b600394c4d18
 
-src/ghdlsynth-beta:
+GHDL_YOSYS_PLUGIN_URL = https://github.com/ghdl/ghdl-yosys-plugin.git
+
+src/ghdl-yosys-plugin:
 	cd $(dir $@) && \
-		git clone $(GHDLSYNTH_URL) $(notdir $@)
+		git clone $(GHDL_YOSYS_PLUGIN_URL) $(notdir $@)
 
-src/ghdlsynth-beta/ghdl.so: | src/ghdlsynth-beta
+src/ghdl-yosys-plugin/ghdl.so: | src/ghdl-yosys-plugin
+	cd $(dir $@) && git checkout $(COMMIT)
 	$(MAKE) -C $(dir $@) all
 
-install: src/ghdlsynth-beta/ghdl.so
+install: src/ghdl-yosys-plugin/ghdl.so
 	sudo $(MAKE) -C $(dir $<) install
 
 dry-run:
-	@echo Will install ghdlsynth from repo:
-	@echo $(GHDLSYNTH_URL)
+	@echo Will install ghdl yosys plugin from repo:
+	@echo $(GHDL_YOSYS_PLUGIN_URL)
 
 all: install
 
