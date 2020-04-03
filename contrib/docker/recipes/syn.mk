99b5851278779fdd1175b6e57301579410b3f8ec
Martin <hackfin@section5.ch>
2020-04-03 15:34:50 +0200

    Submodule info added, tests rules update
    - Added distclean rule to make sure to upload SFX containing recipes
      from git repo

--- contrib/docker/recipes/syn.mk
+++ contrib/docker/recipes/syn.mk
@@ -4,9 +4,13 @@ MASOCIST = src/vhdl/masocist-opensource
 SVFFILE = $(MASOCIST)/syn/versa_ecp5.svf
 
 $(SVFFILE):
-	$(MAKE) -C $(MASOCIST) versa_ecp5-zpu-ghdlsynth
 	$(MAKE) -C $(MASOCIST) sw syn
 
+
+$(MASOCIST)/.config
+	$(MAKE) -C $(MASOCIST) versa_ecp5-zpu-ghdlsynth
+
+all: $(MASOCIST)/.config
 
 test: $(SVFFILE)
 
