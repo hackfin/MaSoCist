8d2d5ac3f1649918b27890a0da8b245af5f9b671
Martin <hackfin@section5.ch>
2020-04-04 16:24:05 +0200

    Buildno fix: make sure to create when non-existing
    - Allows to build according to optimized dependency strategy (make sim)

--- sw/buildno.mk
+++ sw/buildno.mk
@@ -1,8 +1,10 @@
 PROG_BUILDNO = .buildno
 BUILDNO = $(shell cat $(PROG_BUILDNO))
 
-buildnumber:
-	@[ -e  $(PROG_BUILDNO) ] ||  echo 0 > $(PROG_BUILDNO)
+$(PROG_BUILDNO):
+	echo 0 > $@
+
+buildnumber: | $(PROG_BUILDNO)
 	@echo $$(($$(cat $(PROG_BUILDNO)) + 1)) > $(PROG_BUILDNO)
 	@echo Incrementing build number to $$(cat $(PROG_BUILDNO))
 
